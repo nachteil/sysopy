23fe8d78ce9de718b122e0d5e5196e84c6fab424
yorg <nachteil@gmail.com>
2015-03-23 21:28:45 +0100

    initial

---
+++ lab1/list_structs.c
@@ -0,0 +1,6 @@
+//
+// Created by yorg on 23.03.15.
+//
+
+#include "list_structs.h"
+
---
+++ lab1/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(int argc, char * argv[]) {
+
+    list_t * list = create_list();
+
+}
